18da9fd4afcca6ef3e63a20e11681064b3158eca
Ante Coric <corican@cronos.be>
2023-09-24 16:02:26 +0200

    Description fix

--- Program.cs
+++ Program.cs
@@ -1,13 +1,18 @@
 using DynamicLinq;
+using Microsoft.OpenApi.Models;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
-builder.Services.AddControllers();
+builder.Services.AddControllers().AddJsonOptions(opt => opt.JsonSerializerOptions.PropertyNamingPolicy = null);
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(options => options.SwaggerDoc("v1", new OpenApiInfo
+{
+    Title = "DynamicLinq API",
+    Description = "Write queries in the following format 'Property Keyword Value', you can chain queries by separating them with |. Available keywords: is/not/greater/lesser/contains/without. Example: Email contains test"
+}));
 builder.Services.AddSingleton<DataWrapper>();
 
 var app = builder.Build();
